a6d1b96db4d00c243262f84e3ec8993276a2b243
spojchil <2069510482@qq.com>
2026-07-24 01:35:19 +0800

    chore: 移出本地 Paper 运维与退役依赖

--- mcserver/mc.ps1
+++
@@ -1,9 +0,0 @@
-$ErrorActionPreference = 'Stop'
-
-$manager = Join-Path $PSScriptRoot 'mc_manager.py'
-if (-not (Test-Path -LiteralPath $manager)) {
-    throw "Missing manager: $manager"
-}
-
-& python $manager @args
-exit $LASTEXITCODE
